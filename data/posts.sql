37a76312a1ab73644f9068087942b62d214620c2
Troy Neubauer <troyneubauer@gmail.com>
2022-04-03 22:54:18 -0400

    Add first 3 datas

---
+++ data/posts.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO posts (userID, postID) VALUES (1, 1);
+INSERT INTO posts (userID, postID) VALUES (2, 2);
+INSERT INTO posts (userID, postID) VALUES (3, 3);
+INSERT INTO posts (userID, postID) VALUES (4, 4);
+
